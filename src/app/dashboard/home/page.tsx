50e11a47205aa67f89cd915af615c1b6ae41250c
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-08-03 15:45:41 +0000

    page setup

---
+++ src/app/dashboard/home/page.tsx
@@ -0,0 +1,12 @@
+import AppTypography from "@styles/components/appTypography"
+
+const Home = () => {
+    return (
+        <div>
+            <AppTypography>
+                hello
+            </AppTypography>
+        </div>
+    )
+}
+export default Home
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "next/navigation"
+
+const App = () => {
+  redirect('/dashboard/home')
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default App
