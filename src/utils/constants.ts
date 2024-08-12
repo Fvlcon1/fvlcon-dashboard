bbdb749c951c6952f7db059123a6d2ccb3173b88
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-08-12 21:19:56 +0000

    commented out accounts and mfa page for depoyment to go through

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,4 +4,4 @@ export const months = ["January", "February", "March", "April", "May", "June", "
 export const shortMonthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
 export const minimumwidth = 1024
 export const standardMargin = 10
-export const noTempalteRoutes = ['/', '/company-code', '/auth/login', '/auth/signup', '/auth/verification', '/auth/reset-password']
+export const noTempalteRoutes = ['/', '/company-code', '/auth/login', '/auth/signup', '/auth/verification', '/auth/reset-password', '/auth/mfa']
