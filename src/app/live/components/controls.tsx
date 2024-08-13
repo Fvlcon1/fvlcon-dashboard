7477831259fbee9ea48668cd6a8a7f099e54ace6
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-08-13 00:47:00 +0000

    added settings icon to controls of live video feature

--- src/app/live/components/controls.tsx
+++ src/app/live/components/controls.tsx
@@ -1,6 +1,7 @@
 import ClickableTab from "@components/clickable/clickabletab"
 import Flex from "@styles/components/flex"
 import theme from "@styles/theme"
+import { FaGear } from "react-icons/fa6"
 import { IoMdArrowDropdown } from "react-icons/io"
 import { MdViewQuilt } from "react-icons/md"
 
@@ -8,6 +9,8 @@ const Controls = () => {
     return (
         <Flex
             width="fit-content"
+            align="center"
+            gap={0}
         >
             <ClickableTab>
                 <Flex
@@ -15,8 +18,22 @@ const Controls = () => {
                     align='center'
                 >
                     <MdViewQuilt
-                        color={theme.colors.text.primary}
+                        color={theme.colors.text.secondary}
                         size={20}
+                    />
+                    <IoMdArrowDropdown
+                        color={theme.colors.text.secondary}
+                    />
+                </Flex>
+            </ClickableTab>
+            <ClickableTab>
+                <Flex
+                    width="fit-content"
+                    align='center'
+                >
+                    <FaGear
+                        color={theme.colors.text.secondary}
+                        size={17}
                     />
                     <IoMdArrowDropdown
                         color={theme.colors.text.secondary}
