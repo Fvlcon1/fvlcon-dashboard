ba22f1499c463398e3d2866373d8417e321e0bbf
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-08-26 03:02:58 +0000

    used a faster face detection model to be able to detect faces faster

--- src/utils/segmentFaces.ts
+++ src/utils/segmentFaces.ts
@@ -1,32 +1,33 @@
 import * as faceapi from 'face-api.js';
 import { DetailedHTMLProps, ImgHTMLAttributes, MutableRefObject, RefObject, useRef } from 'react';
 
+export const loadModels = async () => {
+  const modelPath = '/model';
+  try {
+    await faceapi.nets.tinyFaceDetector.loadFromUri(modelPath);
+    await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
+    await faceapi.nets.faceRecognitionNet.loadFromUri(modelPath);
+    await faceapi.nets.ageGenderNet.loadFromUri(modelPath);
+    console.log('Models loaded');
+  } catch (error) {
+    console.error('Error loading models:', error);
+  }
+};
+
 const segmentFaces = async (url : string, imageRef : RefObject<HTMLImageElement>, image? : DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>) => {
   try {
     const img = await faceapi.fetchImage(url);
     console.log({img})
-    const loadModels = async () => {
-        const modelPath = '/model';
-        try {
-          await faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath);
-          await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
-          await faceapi.nets.faceRecognitionNet.loadFromUri(modelPath);
-          await faceapi.nets.ageGenderNet.loadFromUri(modelPath);
-          console.log('Models loaded');
-        } catch (error) {
-          console.error('Error loading models:', error);
-        }
-    };
     await loadModels();
   
     if (
-      faceapi.nets.ssdMobilenetv1.isLoaded &&
+      faceapi.nets.tinyFaceDetector.isLoaded &&
       faceapi.nets.faceLandmark68Net.isLoaded &&
       faceapi.nets.faceRecognitionNet.isLoaded &&
       faceapi.nets.ageGenderNet.isLoaded
     ) {
     const detections = await faceapi
-      .detectAllFaces(img, new faceapi.SsdMobilenetv1Options())
+      .detectAllFaces(img, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceDescriptors()
       .withAgeAndGender();
