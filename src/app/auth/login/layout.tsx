4d31ac196bf7c2726bf4b5166913098d87ab08d2
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-10-14 21:48:56 +0000

    added jwt authentication

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from 'react'
 
 export default function RootLayout({
   children,
@@ -9,10 +10,12 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <SessionProvider>
-      <div className="w-full max-w-[1500px]">
-        {children}
-      </div>
-    </SessionProvider>
+    <Suspense>
+      <SessionProvider>
+        <div className="w-full max-w-[1500px]">
+          {children}
+        </div>
+      </SessionProvider>
+    </Suspense>
   );
 }
