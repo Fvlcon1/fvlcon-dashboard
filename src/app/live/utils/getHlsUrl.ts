f0d027cb54e5cfd49a837d68d415187ac0adaa1f
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-10-16 23:11:17 +0000

    bug fix

--- src/app/live/utils/getHlsUrl.ts
+++ src/app/live/utils/getHlsUrl.ts
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk';
 
 AWS.config.update({
     region: "us-east-1",
-    accessKeyId: process.env.AKIAU6GDZN7ULFFYL5NA,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_KEY_ID,
   });
 
