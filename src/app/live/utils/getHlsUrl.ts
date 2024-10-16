b4b2c1f30601eeb76114c7c61dd1937b64febc62
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-10-16 23:48:04 +0000

    added prefix NEXT_PUBLIC_ to env variables

--- src/app/live/utils/getHlsUrl.ts
+++ src/app/live/utils/getHlsUrl.ts
@@ -1,42 +1,54 @@
 import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Correct way to load environment variables
+
+const accessKeyId = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.NEXT_PUBLIC_AWS_SECRET_KEY_ID;
+
+console.log({ jwt: process.env.JWT_SECRET });
+console.log({ accessKeyId, secretAccessKey });
 
 AWS.config.update({
-    region: "us-east-1",
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY_ID,
-  });
+    region: 'us-east-1',
+    accessKeyId,
+    secretAccessKey,
+});
 
-  // Initialize KVS client
+// Initialize KVS client
 const kinesisVideo = new AWS.KinesisVideo({
-    region: "us-east-1",
+    region: 'us-east-1',
 });
 
 // Get HLS Streaming URL from Kinesis Video Streams
-export const getHLSStreamURL = async (streamName : string) => {
+export const getHLSStreamURL = async (streamName: string) => {
     try {
         const params = {
             StreamName: streamName,
-            PlaybackMode: "LIVE", // For live streaming
+            PlaybackMode: 'LIVE', // For live streaming
         };
 
         // Get the endpoint for HLS session
-        const endpointResponse = await kinesisVideo.getDataEndpoint({
-            StreamName: streamName,
-            APIName: "GET_HLS_STREAMING_SESSION_URL",
-        }).promise();
+        const endpointResponse = await kinesisVideo
+            .getDataEndpoint({
+                StreamName: streamName,
+                APIName: 'GET_HLS_STREAMING_SESSION_URL',
+            })
+            .promise();
 
         const kinesisVideoArchivedMedia = new AWS.KinesisVideoArchivedMedia({
             endpoint: endpointResponse.DataEndpoint,
-            region: "us-east-1",
+            region: 'us-east-1',
         });
 
         // Get the HLS session URL
         const hlsResponse = await kinesisVideoArchivedMedia
             .getHLSStreamingSessionURL(params)
             .promise();
-        console.log({url : hlsResponse.HLSStreamingSessionURL})
+        
+        console.log({ url: hlsResponse.HLSStreamingSessionURL });
         return hlsResponse.HLSStreamingSessionURL;
     } catch (error) {
-        console.error("Error getting HLS stream URL:", error);
+        console.error('Error getting HLS stream URL:', error);
     }
-}
+};
--- src/context/live.tsx
+++ src/context/live.tsx
@@ -30,27 +30,27 @@ export const LiveProvider = ({ children }: { children: ReactNode }) => {
             location : 'Osu Accra',
             streamName : "my_stream_name",
             coordinates : [5.558, -0.187]
-        },
-        {
-            id : '2',
-            url : "https://stream.fvlcon.app:8888/towncouncil/channel_3",
-            location : 'East legon, Accra',
-            streamName : "my_stream_name",
-            coordinates : [5.65, -0.163]
-        },
-        {
-            id : '4',
-            url : "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-            location : 'Spintex, Accra',
-            streamName : "my_stream_name",
-            coordinates : [5.6316, -0.1229]
-        },
-        {
-            id : '5',
-            url : "https://b-61687957.kinesisvideo.us-east-1.amazonaws.com/hls/v1/getHLSMasterPlaylist.m3u8?SessionToken=CiBvnJwsJ3RF4OqXt1l1mfRt1CFrQtcZZ0tbS4YDHWHKZxIQ-1eHThDwhJkmxnuCbigpVhoZAYljNZaUescsE4aPFKDxg6bMwCB0d7ZhwSIgf5NZ0Nfo7dV0E0b2IIXZjpZiMXOoMz69k6tdK9_QOz4~",
-            location : 'Kokomlemle, new town',
-            streamName : "my_stream_name",
-            coordinates : [5.5655, -0.2077]
+        // },
+        // {
+        //     id : '2',
+        //     url : "https://stream.fvlcon.app:8888/towncouncil/channel_3",
+        //     location : 'East legon, Accra',
+        //     streamName : "my_stream_name",
+        //     coordinates : [5.65, -0.163]
+        // },
+        // {
+        //     id : '4',
+        //     url : "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        //     location : 'Spintex, Accra',
+        //     streamName : "my_stream_name",
+        //     coordinates : [5.6316, -0.1229]
+        // },
+        // {
+        //     id : '5',
+        //     url : "https://b-61687957.kinesisvideo.us-east-1.amazonaws.com/hls/v1/getHLSMasterPlaylist.m3u8?SessionToken=CiBvnJwsJ3RF4OqXt1l1mfRt1CFrQtcZZ0tbS4YDHWHKZxIQ-1eHThDwhJkmxnuCbigpVhoZAYljNZaUescsE4aPFKDxg6bMwCB0d7ZhwSIgf5NZ0Nfo7dV0E0b2IIXZjpZiMXOoMz69k6tdK9_QOz4~",
+        //     location : 'Kokomlemle, new town',
+        //     streamName : "my_stream_name",
+        //     coordinates : [5.5655, -0.2077]
         },
     ])
     const [numberOfCamerasPerPage, setNumberOfCamerasPerPage] = useState<number>(4)
