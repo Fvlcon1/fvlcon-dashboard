4ea9183e7b434c1cd5b520e2a952cd8b8fcf6818
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-11-20 08:42:44 +0000

    login error checking

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -69,6 +69,7 @@ export default function LoginForm() {
         companyCode,
       })
   
+      console.log({result})
       if(!result?.error){
         router.push(`/agreementpage`);
       } else {
