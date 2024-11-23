95c1fb1c76635a18b09e51da725a37cc9211c2aa
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-11-23 11:37:19 +0000

    bug fix

--- src/app/live/components/live container/liveContainer.tsx
+++ src/app/live/components/live container/liveContainer.tsx
@@ -107,7 +107,7 @@ const LiveContainer = ({
                     height : `${liveHeight}px`
                 }}
             >
-                <iframe 
+                {/* <iframe 
                     src={url}
                     title="stream"
                     width={'100%'}
@@ -115,8 +115,8 @@ const LiveContainer = ({
                     id={id}
                     allowFullScreen
                     allow="accelerometer; autoplay; clipboard-write"
-                />
-                {/* <div id={id} className="w-full h-full relative">
+                /> */}
+                <div id={id} className="w-full h-full relative">
                     <div id={`CanvasContainer${id}`} className="canvasContainer w-full h-full absolute pointer-events-none z-10 top-0 left-0">
 
                     </div>
@@ -129,7 +129,7 @@ const LiveContainer = ({
                         placeholder="Stream"
                         autoPlay={true}
                     />
-                </div> */}
+                </div>
             </div>
         </div>
     )
