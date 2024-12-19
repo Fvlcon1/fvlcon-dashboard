7f08a319fbd91b56c67818f3f52a6e8ec4612f17
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-12-19 15:11:46 +0000

    remove vercel's image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images : {
+    unoptimized : true
+  },
     webpack: (config, { isServer }) => {
         if (!isServer) {
           config.resolve.fallback = {
