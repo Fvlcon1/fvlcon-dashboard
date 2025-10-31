9d248f8f7380ed8a7a0bec8c455b5ffe623d704c
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2025-10-31 19:31:59 +0000

    - Update session age

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -8,66 +8,66 @@ import prisma from '@/lib/prisma'; // Adjust based on your setup
 import dotenv from 'dotenv';
 
 export const authOptions: NextAuthOptions = {
-  debug: process.env.NODE_ENV === 'development',
+	debug: process.env.NODE_ENV === 'development',
 
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "your-email" },
-        password: { label: "Password", type: "password", placeholder: "your-password" },
-        companyCode: { label: "Company Code", type: "text", placeholder: "your-company-code" }
-      },
-      authorize: async (credentials) => {
-        try {
-          if (!credentials?.email || !credentials.password || !credentials.companyCode) return null;
-    
-          const user = await prisma.user.findUnique({ where: { email : credentials.email } });
-          if (user && user.companyCode === credentials.companyCode) {
-            const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
-            if (isPasswordValid){
-              return user
-            };
-            return null
-          } else {
-            return null
-          }
-        } catch (error) {
-          console.log({error})
-          return null
-        }
-      },
-    }),
-  ],
+	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				email: { label: "Email", type: "text", placeholder: "your-email" },
+				password: { label: "Password", type: "password", placeholder: "your-password" },
+				companyCode: { label: "Company Code", type: "text", placeholder: "your-company-code" }
+			},
+			authorize: async (credentials) => {
+				try {
+					if (!credentials?.email || !credentials.password || !credentials.companyCode) return null;
 
-  session: {
-    strategy: 'jwt',
-    maxAge: 60 * 60, // 1 hour
-  },
+					const user = await prisma.user.findUnique({ where: { email: credentials.email } });
+					if (user && user.companyCode === credentials.companyCode) {
+						const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
+						if (isPasswordValid) {
+							return user
+						};
+						return null
+					} else {
+						return null
+					}
+				} catch (error) {
+					console.log({ error })
+					return null
+				}
+			},
+		}),
+	],
 
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+	session: {
+		strategy: 'jwt',
+		maxAge: 60 * 60 * 24, // 1 day
+	},
 
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
+	secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-          token.email = user.email;
-          token.userId = user.id;  // Add userId to the token
-      }
-      return token;
-  },
-    session: async ({ session, token }) => {
-      if (session.user) {
-        session.user.email = token.email;
-        session.user.userId = token.userId;
-        session.token = token
-      }
-      return session;
-    },
-  },
+	jwt: {
+		secret: process.env.JWT_SECRET,
+	},
 
-  adapter: PrismaAdapter(prisma),
+	callbacks: {
+		jwt: async ({ token, user }) => {
+			if (user) {
+				token.email = user.email;
+				token.userId = user.id;  // Add userId to the token
+			}
+			return token;
+		},
+		session: async ({ session, token }) => {
+			if (session.user) {
+				session.user.email = token.email;
+				session.user.userId = token.userId;
+				session.token = token
+			}
+			return session;
+		},
+	},
+
+	adapter: PrismaAdapter(prisma),
 };
